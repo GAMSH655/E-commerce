1272563a93f72feec7401d68bab9fafaaff8db53
ABDULQUADRI <shittuabdquadri0@gmail.com>
2024-03-29 06:36:14 -0700

    working in the hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,7 +2,19 @@ import "./Hero.css"
 function Hero(){
      return (
          <div>
-            
+            <div className="textBox">
+                <p className="heroText">woman;s fashion</p>
+                <p className="heroText">men fashion</p>
+                <p className="heroText">electronics</p>
+                <p className="heroText">home and lifestyle</p>
+                <p className="heroText">medicine</p>
+                <p className="heroText">sport & outdoor</p>
+                <p className="heroText">baby's toy</p>
+                <p className="heroText">groceries and pets</p>
+                <p className="heroText">health & beauty</p>
+                
+                
+            </div>
          </div>
      )
 } export default Hero
