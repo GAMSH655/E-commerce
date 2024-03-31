6bb81fdf44f34b6c2294c1bc654d185ae89103de
ABDULQUADRI <shittuabdquadri0@gmail.com>
2024-03-31 09:25:47 -0700

    building the footer section

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,24 @@
-import FooterStyle from "../Components/Header"
+import FooterStyle from "../Components/Footer.module.css"
 function Footer(){
      return(
          <div>
-             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias enim ab laborum iure ea dolorum, libero cupiditate possimus dignissimos quae officiis perspiciatis error atque molestiae assumenda omnis quas vitae sapiente.</p>
+           <div className={FooterStyle.footerContainer}>
+              {/* start of first container */}
+            <div className={FooterStyle.firstcont}>
+                <h3 className={FooterStyle.exclusive}>
+                    exclusive
+                </h3>
+                <p className={FooterStyle.sub}>
+                    suscribe
+                </p>
+                <p className={FooterStyle.get}>
+                  get 10%
+                </p>
+                <input type="email" className={FooterStyle.FooterInpt} 
+                placeholder="enter your email"/>
+            </div> 
+            {/* end of first container */}
+           </div>   
          </div>
      )
 }export default Footer
