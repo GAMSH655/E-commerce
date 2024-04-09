d4e212e82bbe004c1f4c9e03fba664e55d6a40fb
ABDULQUADRI <shittuabdquadri0@gmail.com>
2024-04-09 07:56:51 -0700

    happ eid in advance

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,6 @@ import Bar from "../assets/Bar.png"
 function Footer(){
      return(
          <div>
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
            <div className={FooterStyle.footerContainer}>
               {/* start of first container */}
             <div className={FooterStyle.firstcont}>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,23 @@
 import "./Header.css"
 import { NavLink } from "react-router-dom"
+import { FaHome  , FaPhone , FaUser ,FaFile  , FaTimes} from "react-icons/fa"
+import { FaBars } from "react-icons/fa"
+import { useState } from "react"
+
 function Header(){
+    const [isOpen , setIsOpen] = useState(true)
+
+    function handClick(){
+        setIsOpen(!isOpen)
+    }
+    
     return(
-         <div className="Header">
+            <div>
+            <div className="Header">
             <h3 className="logoText">exclusive</h3>
             <div className="Navcontainer">
                  <NavLink to ="/" className="Link" >home</NavLink>
-                  <NavLink to="/contact" className="Link">Contact</NavLink>
+                  <NavLink to="/contact" className="Link">Contact</NavLink >
                   <NavLink to="/about" className="Link">About</NavLink>
                   <NavLink to="/sign" className="Link">Sign up</NavLink>
             </div>
@@ -18,5 +29,28 @@ function Header(){
              <i className="fa-solid fa-cart-shopping"></i>
              </div>
          </div>
+             {/* mobileHeader */}
+         <div className="MobileHeader">
+            <h3 className="MoblogoText">exclusive</h3>
+            <span>
+              <input type="" placeholder="what are looking for " className="Mobinput"/><i className="fa-solid fa-magnifying-glass "></i>
+              </span>
+            <button className="openBtn" onClick={handClick}>
+                {isOpen ? <FaTimes/> :  <FaBars/>}
+                </button>
+         </div>
+            { isOpen && (
+                         <div className="MobileNavcontainer">
+                         <div className="searchContainer">
+                             <i className="fa-solid fa-heart"></i>
+                             <i className="fa-solid fa-cart-shopping"></i>
+                             </div>
+                                 <NavLink to ="/" className="MobileLink" >home <FaHome className="mobIcon"/> </NavLink>
+                                  <NavLink to="/contact" className="MobileLink">Contact<FaPhone  className="mobIcon"/> </NavLink>
+                                  <NavLink to="/about" className="MobileLink">About<FaUser  className="mobIcon"/> </NavLink>
+                                  <NavLink to="/sign" className="MobileLink">Sign up <FaFile  className="mobIcon" /></NavLink>
+                            </div>
+            )}
+            </div>
     )
 }export default Header
