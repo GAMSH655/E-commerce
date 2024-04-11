c11fa929768f3afca06248c4f712f9bfb5dad6d9
ABDULQUADRI <shittuabdquadri0@gmail.com>
2024-04-11 08:25:28 -0700

    moving

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,6 +1,7 @@
 import "../Components/Contact.css"
 import Header from "../Components/Header"
 import { FaPhone } from "react-icons/fa"
+import { FaEnvelope } from "react-icons/fa"
 function Contact(){
      return(
         <div>
@@ -21,9 +22,21 @@ function Contact(){
                 </span>
               </p>
                </div>
+
+               <p className="envlope">
+                  <span className="envIcon">
+                       <FaEnvelope/> 
+                  </span>
+                  Write To US
+                 </p>
+                 <p className="fill">
+                 Fill out our form and we will contact you within 24 hours.
+                 </p>
+                 <p className="email">
+                 Emails: customer@exclusive.com
+                 </p>
              </div>
              <div className="gridCont2">
-               
              </div>
            </div>
         </div>
