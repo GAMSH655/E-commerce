07edf24801ce02f36201b876742798fb30ff149c
ABDULQUADRI <shittuabdquadri0@gmail.com>
2024-04-14 18:16:21 -0700

    up and working

--- src/Pages/Billing.jsx
+++ src/Pages/Billing.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from "../Components/Header"
+import "../Components/Billing.css"
 const Billing = () => {
   return (
     <div>
@@ -8,23 +9,30 @@ const Billing = () => {
         <h3 className="billinDetails">billing detail </h3>
         <div className="billingFormContainer">
              <form action="">
-                <h3 className="firstNAme">first name</h3>
-                <input type="text" />
-                <h3 className="comapnayName">company name</h3>
-                <input type="text" />
-                <h3 className="street">company name</h3>
-                <input type="text" />
-                <h3>town / city</h3>
-                <select name="town" id="cars">
+                <h3 className="name">first name</h3>
+                <input type="text" className='billing_input' />
+                <br />
+                <h3 className="name">company name</h3>
+                <input type="text" className='billing_input'  />
+                <br />
+                <h3 className="name">company name</h3>
+                <input type="text" className='billing_input'  />
+                <br />
+                <h3 className="name">town / city</h3>
+                <select name="town" id="cars" className='billing_input' >
                     <option value="abia">abia</option>
                     <option value="umaya">umaya</option>
                     <option value="lagos">lago</option>
                     <option value="abuja">abuja</option>
                 </select>
-               <h3 className='phone'>phone number</h3>
-               <input type="number" />
-               <h3 className='phone'>email address</h3>
-               <input type="email" />
+               <h3 className="name">phone number</h3>
+               <input type="number" className='billing_input' />
+               <br />
+               <h3 className="name">email address</h3>
+               <input type="email"className='billing_input'  />
+               <br />
+               <input type="checkbox"className='radio' />
+               <label htmlFor="Save this information for faster check-out next time" > Save this information for faster check-out next time</label>
              </form>
         </div>
         </div>
